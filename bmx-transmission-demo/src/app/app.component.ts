701a515fdca8f4ab6cfe864632d7ad980bd451c8
Deep-Dey <deepdey20998@gmail.com>
2023-05-12 00:59:56 +0530

    add transmission-service in demo

--- bmx-transmission-demo/src/app/app.component.ts
+++ bmx-transmission-demo/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { BmxTransmissionService } from 'bmx-transmission/public-api';
 
 @Component({
-  selector: 'bmx-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'bmx-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bmx-transmission-demo';
+    
+    title = 'bmx-transmission-demo';
+
+    constructor(
+        private transmission: BmxTransmissionService
+    ) {}
 }
